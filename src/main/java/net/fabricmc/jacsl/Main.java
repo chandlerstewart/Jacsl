5a76c412d82802b34ea8cfd22ea6aadfab599244
chandlerstewart <65257699+chandlerstewart@users.noreply.github.com>
2021-09-30 20:34:38 -0500

    initial commit

---
+++ src/main/java/net/fabricmc/jacsl/Main.java
@@ -0,0 +1,35 @@
+package net.fabricmc.jacsl;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.jacsl.items.NewItem;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+
+public class Main implements ModInitializer {
+
+	public static final String MODID = "jacsl";
+
+	public static final ItemGroup NEW_ITEM_GROUP = FabricItemGroupBuilder.build(
+			new Identifier(MODID, "general"),
+			() -> new ItemStack(Main.NEW_ITEM));
+
+	public static final Item NEW_ITEM = new NewItem(new FabricItemSettings().group(Main.NEW_ITEM_GROUP));
+
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		Registry.register(Registry.ITEM, new Identifier(MODID, "new_item"), NEW_ITEM);
+		System.out.println("Hello Fabric world!");
+	}
+}
