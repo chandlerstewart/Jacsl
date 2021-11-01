c81726af99308e692591cb62d779c3550cd401f7
thejoshuacox <joshuawaynecox@gmail.com>
2021-11-01 17:44:33 -0500

    Created aerogel block, still need to do item drops for it though, and name it correctly

--- src/main/java/net/fabricmc/jacsl/Main.java
+++ src/main/java/net/fabricmc/jacsl/Main.java
@@ -3,11 +3,14 @@ package net.fabricmc.jacsl;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.jacsl.enchantments.KnockbackiumEnchantment;
 import net.fabricmc.jacsl.items.AerogelSword;
 import net.fabricmc.jacsl.items.NewItem;
 import net.fabricmc.jacsl.items.DiskOfTeleportation;
 import net.fabricmc.jacsl.materials.Aerogel;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -29,6 +32,8 @@ public class Main implements ModInitializer {
 	public static final Item TELEPORT_ITEM = new DiskOfTeleportation(new FabricItemSettings().group(Main.NEW_ITEM_GROUP));
 	public static final ToolItem AEROGEL_SWORD = new AerogelSword(Aerogel.INSTANCE, 1, 2f, new FabricItemSettings().group(Main.NEW_ITEM_GROUP));
 
+	public static final Block AEROGEL_CHUNKS = new Block(FabricBlockSettings.of(Material.SOIL).hardness(1.0f).strength(1200.0f));
+
 	private static Enchantment KNOCKBACK_ENCHANTMENT = Registry.register(Registry.ENCHANTMENT, new Identifier(MODID, "knockbackium_enchantment"), new KnockbackiumEnchantment());
 
 
@@ -41,6 +46,10 @@ public class Main implements ModInitializer {
 		Registry.register(Registry.ITEM, new Identifier(MODID, "new_item"), NEW_ITEM);
 		Registry.register(Registry.ITEM, new Identifier(MODID, "disk_of_teleportation"), TELEPORT_ITEM);
 		Registry.register(Registry.ITEM, new Identifier(MODID, "aerogel_sword"), AEROGEL_SWORD);
+
+		//Registering the aerogel blocks
+		Registry.register(Registry.BLOCK, new Identifier(MODID, "aerogel_chunks"), AEROGEL_CHUNKS);
+		Registry.register(Registry.ITEM, new Identifier(MODID, "aerogel_chunks"), new BlockItem(AEROGEL_CHUNKS, new FabricItemSettings().group(Main.NEW_ITEM_GROUP)));
 
 		System.out.println("Hello Fabric world!");
 	}
