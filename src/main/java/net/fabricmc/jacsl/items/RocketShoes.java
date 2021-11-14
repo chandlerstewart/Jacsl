52b4e31648c112ebf106a3513ca02d8eb5fce3f6
Alex Lopez <alex-lopez1997@hotmail.com>
2021-11-14 00:12:40 -0600

    added rocket ingot, added two pics, worked on boots

--- src/main/java/net/fabricmc/jacsl/items/RocketShoes.java
+++ src/main/java/net/fabricmc/jacsl/items/RocketShoes.java
@@ -1,20 +1,13 @@
 package net.fabricmc.jacsl.items;
 
-import net.fabricmc.jacsl.Main;
-import net.fabricmc.jacsl.entities.TeleportItemEntity;
-import net.minecraft.client.item.TooltipContext;
+import net.fabricmc.jacsl.materials.Rocket;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.text.LiteralText;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
@@ -51,10 +44,10 @@ public class RocketShoes extends ArmorItem
         ItemStack itemStack = user.getStackInHand(hand);
         EquipmentSlot equipmentSlot = MobEntity.getPreferredEquipmentSlot(itemStack);
         ItemStack itemStack2 = user.getEquippedStack(equipmentSlot);
+        user.setVelocity(user.getVelocity().x,.5,user.getVelocity().z);
+        user.playSound(SoundEvents.ENTITY_FIREWORK_ROCKET_LAUNCH, 1.0F, 1.0F);
         if (itemStack2.isEmpty()) {
             user.equipStack(equipmentSlot, itemStack.copy());
-            user.setVelocity(user.getVelocity().x,.5,user.getVelocity().z);
-            user.playSound(SoundEvents.ENTITY_FIREWORK_ROCKET_LAUNCH, 1.0F, 1.0F);
             itemStack.setCount(0);
             return TypedActionResult.success(itemStack, world.isClient());
         } else {
