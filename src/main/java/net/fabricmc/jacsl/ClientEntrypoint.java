8f4515fc1835c9cb380c8c51f6654f3cf1278e83
Alex Lopez <alex-lopez1997@hotmail.com>
2021-11-15 00:52:36 -0600

    keybinding

--- src/main/java/net/fabricmc/jacsl/ClientEntrypoint.java
+++ src/main/java/net/fabricmc/jacsl/ClientEntrypoint.java
@@ -7,14 +7,22 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.LiteralText;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
 import org.lwjgl.glfw.GLFW;
 
 public class ClientEntrypoint implements ClientModInitializer {
 
     private static KeyBinding keybinding;
-
+    private static World world;
+    private static Hand hand;
 
     @Override
     public void onInitializeClient() {
@@ -27,9 +35,14 @@ public class ClientEntrypoint implements ClientModInitializer {
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (keybinding.wasPressed()) {
-                System.out.println("AAAAAAAAAA");
+            while (keybinding.wasPressed())
+            {
+                PlayerEntity user = MinecraftClient.getInstance().player;
+                float pitch = 1.5f / ((float) Math.random() * .4f + .8f);
+                user.setVelocity(user.getVelocity().x,.5f,user.getVelocity().z);
+                user.playSound(SoundEvents.ENTITY_FIREWORK_ROCKET_LAUNCH, 1.0f, pitch);
             }
         });
     }
+
 }
--- src/main/java/net/fabricmc/jacsl/items/RocketShoes.java
+++ src/main/java/net/fabricmc/jacsl/items/RocketShoes.java
@@ -1,37 +1,8 @@
 package net.fabricmc.jacsl.items;
 
-//import net.fabricmc.jacsl.materials.Rocket;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
-import net.minecraft.item.ItemStack;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
-import net.minecraft.world.World;
-
-import java.util.List;
-
-/*
-public class RocketShoes extends Item{
-    public RocketShoes(Settings settings) {
-        super(settings);
-    }
-    @Override
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand){
-
-        playerEntity.setVelocity(playerEntity.getVelocity().x,.5,playerEntity.getVelocity().z);
-        playerEntity.playSound(SoundEvents.ENTITY_FIREWORK_ROCKET_LAUNCH, 1.0F, 1.0F);
-        return TypedActionResult.success(playerEntity.getStackInHand(hand));
-    }
-
-    public void appendToolTip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext)
-    {
-        tooltip.add(new TranslatableText("These shoes can make you fly"));
-    }
-}*/
 
 public class RocketShoes extends ArmorItem
 {
@@ -40,12 +11,11 @@ public class RocketShoes extends ArmorItem
         super(material, slot, settings);
     }
 
+    /*
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         ItemStack itemStack = user.getStackInHand(hand);
         EquipmentSlot equipmentSlot = MobEntity.getPreferredEquipmentSlot(itemStack);
         ItemStack itemStack2 = user.getEquippedStack(equipmentSlot);
-        user.setVelocity(user.getVelocity().x,.5,user.getVelocity().z);
-        user.playSound(SoundEvents.ENTITY_FIREWORK_ROCKET_LAUNCH, 1.0F, 1.0F);
         if (itemStack2.isEmpty()) {
             user.equipStack(equipmentSlot, itemStack.copy());
             itemStack.setCount(0);
@@ -53,5 +23,5 @@ public class RocketShoes extends ArmorItem
         } else {
             return TypedActionResult.fail(itemStack);
         }
-    }
+    }*/
 }
