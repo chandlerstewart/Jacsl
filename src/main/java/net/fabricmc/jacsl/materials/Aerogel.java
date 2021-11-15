28c962815649307dcdc877190be11d5b163130da
thejoshuacox <joshuawaynecox@gmail.com>
2021-11-14 22:39:59 -0600

    Just removed some comments

--- src/main/java/net/fabricmc/jacsl/materials/Aerogel.java
+++ src/main/java/net/fabricmc/jacsl/materials/Aerogel.java
@@ -39,29 +39,5 @@ public class Aerogel implements ToolMaterial {
         return Ingredient.ofItems(Main.AEROGEL_CUBE);
     }
 
-    /*public Aerogel(Settings settings) {
-        super(settings);
-    }*/
-
-    /*@Override
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand){
-
-        double xVel = playerEntity.getVelocity().x;
-        double yVel = playerEntity.getVelocity().y;
-        double zVel = playerEntity.getVelocity().z;
-        float pitch = 0.5f/(RANDOM.nextFloat()*.4f + .8f);
-
-        playerEntity.setVelocity(xVel, yVel + 0.5, zVel);
-        playerEntity.playSound(SoundEvents.ENTITY_PLAYER_ATTACK_SWEEP, 1.0f, pitch);
-
-        return TypedActionResult.success(playerEntity.getStackInHand(hand));
-    }*/
-
-    /*@Override
-    public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext)
-    {
-        tooltip.add(new TranslatableText("Wack people."));
-        tooltip.add(new TranslatableText("HARD").formatted(Formatting.DARK_RED));
-    }*/
 
 }
