0262cfc74b71d48147cd3f1bb37b34c76b60aa49
chandlerstewart <65257699+chandlerstewart@users.noreply.github.com>
2021-11-15 00:10:37 -0600

    rocket shoes client

---
+++ src/main/java/net/fabricmc/jacsl/ClientEntrypoint.java
@@ -0,0 +1,36 @@
+package net.fabricmc.jacsl;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.LiteralText;
+import org.lwjgl.glfw.GLFW;
+
+public class ClientEntrypoint implements ClientModInitializer {
+
+    private static KeyBinding keybinding;
+
+
+    @Override
+    public void onInitializeClient() {
+
+
+        keybinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.jacsl.rocketboots",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_J,
+                "category.jacsl.custom"
+        ));
+
+        //ClientTickEvents.END_CLIENT_TICK.register(client -> {
+         //   while (keybinding.wasPressed()) {
+
+        //    }
+        //});
+    }
+}
